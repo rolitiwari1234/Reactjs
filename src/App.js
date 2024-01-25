b41a6fa941fe5d0223e95a469315c5f41f274716
rolitiwari1234 <aniltiwari9917975244@gmail.com>
2024-01-25 13:45:40 +0530

    state with class component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import {useState} from 'react'
+import React ,{Component} from 'react'
 import logo from './logo.svg';
 import './App.css';
 
 
+class App extends Component{
 
-function App() {
- const [data,setData] = useState(0)
- function updateData (){
-  setData ( data+1)
- }
- console.warn("______")
+  constructor(){
+    super();
+    this.state={
+      data :1
+    }
+  }
+  apple(){
+    this.setState({data:this.state.data+1})
+  }
+render (){
 return (
     <div className="App">
-     <h1>{data}</h1>
-     <button onClick = {updateData}>update data</button> 
+     <h1>{this.state.data}</h1>
+     <button onClick={()=>this.apple()}>click me</button>
     </div>
   );
 }
-
+}
 export default App;
