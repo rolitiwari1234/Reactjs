6749a049f12224e60bdb674eaaf4d3598a2f6784
rolitiwari1234 <aniltiwari9917975244@gmail.com>
2024-01-26 20:53:49 +0530

    handle form

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
-import React from 'react'
+import {useState} from 'react'
 import logo from './logo.svg';
 import './App.css';
 
 function App(){
-  const [status,setStatus] = React.useState(true)
+  const [name,setName] = useState("")
+  const [tnc,setTnc] = useState(false)
+  const [interest,setInterest] = useState("")
+  function getFormData(e)
+  {
+    console.warn(name,tnc,interest)
+   e.preventDefault()
+  }
   
 return (
     <div className="App">
-     {
-      status ?<h1>hello world !</h1> : null
-     }
-     {/* <button onClick = {()=>setStatus(false)}>hide</button>
-     <button onClick = {()=>setStatus(true)}>show</button> */}
-     <button onClick = {()=>setStatus(!status)}>toogle</button>
+     <h2>Handle from in react</h2>
+     <form onSubmit = {getFormData}>
+     <input type = "text" placeholder="enter name"value = {name} onChange={(e)=>setName(e.target.value)}/><br/><br/>
+     <select onChange={(e)=>setInterest(e.target.value)}>
+      <option>select option</option>
+      <option>marvel</option>
+      <option>cd</option>
+     </select><br/><br/>
+     <input type = "checkbox" onChange={(e)=>setTnc(e.target.checked)}/><span>Accept term and conditional</span><br/><br/>
+     <button type = "submit">submit</button>
+     <button >clear</button>
+      </form>
      </div>
     )
 }
