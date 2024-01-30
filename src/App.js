0793a51969ccd17fd675f23a2d30b46236def338
rolitiwari1234 <aniltiwari9917975244@gmail.com>
2024-01-30 21:08:33 +0530

    array listing with map function

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import React from 'react'
 import './App.css';
-import './style.css'
-import style from './custom.module.css'
+
 function App (){
-  
-   
-  return(
+  // const students = ["roli","yashi","ryg","hgg"];
+  const students = [
+    {name : "roli",gmail : "roli@gmail.com",contact : 333 },
+    {name : "yashi",gmail : "yashi@gmail.com",contact : 111},
+    {name : "tiwari",gmail : "tiwari@gmail.com",contact:222}
+  ]
+ return(
    <div className='App'>
-   <h1 className='prime'>style 1 in react js</h1>
-    <h1 style = {{backgroundColor:"red",color:"blue"}} >style 1 in react js</h1> 
-    <h1 className = {style.h1}>style 1 in react js</h1>
-   </div>
+   <h2>install bootstrap</h2>
+   <table border = "1">
+    <tr>
+      <td>name</td>
+      <td>gmail</td>
+      <td>contact</td>
+    </tr>
+  {
+    students.map((data)=>
+    <tr> 
+      <td> {data.name}</td>
+      <td>{data.gmail}</td>
+      <td>{data.contact}</td>
+        </tr>
     )
   }
-  
+  </table>
+  </div>
+      )}
    
- export default App;
+   export default App;
  
